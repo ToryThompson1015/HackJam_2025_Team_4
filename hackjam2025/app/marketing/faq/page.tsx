40795ba21881305e6fca4b2d4a4a5e050966a782
AriMar159 <96703023+AriMar159@users.noreply.github.com>
2025-07-12 00:03:28 -0400

    adding work completed

---
+++ hackjam2025/app/marketing/faq/page.tsx
@@ -0,0 +1,9 @@
+const FAQPage = () => {
+  return (
+    <div>
+      <h1>Frequently Asked Questions</h1>
+      <p>Here you will find answers to the most common questions.</p>
+      {/* Add your FAQ content here */}
+    </div>
+  );
+}
---
+++ hackjam2025/app/page.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button>Primary Outline</Button>
+    </div>
+  );
+}
